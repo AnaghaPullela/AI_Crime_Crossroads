999c60b35c302b6ba5b802e2f9789ecabc5c12b4
BalaSham <shambavi.balachandran@gmail.com>
2025-04-12 14:10:46 -0500

    edits

--- data_utils.py
+++ data_utils.py
@@ -1,4 +1,5 @@
 import json
+import folium;
 
 with open("cleaned_crimes.json") as f:
     data = json.load(f)
@@ -7,3 +8,11 @@ locations = []
 
 for item in data:
     locations.append((item['LATITUDE'], item['LONGITUDE']))
+
+
+my_map = folium.Map(location=[41.8781, - 87.6298], zoom_start= 10)
+
+for item in locations:
+    folium.Marker(item, popup='Marker Label').add_to(my_map)
+
+my_map.save("crime_map.html")
